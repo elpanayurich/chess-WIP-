00dbec54e7c2ccb8370ed27e16bad41c73862696
elpanayurich <urielgbarbulo@gmail.com>
2026-02-22 19:23:47 +0100

    feat: check stuff

--- game.js
+++ game.js
@@ -4,6 +4,11 @@ let occupied_squares_black = [];
 let eaten_black = [];
 let eaten_white = [];
 
+let attacked_squares_white = [];
+let attacked_squares_black = [];
+
+let in_check;
+
 let castle_black_1 = true;
 let castle_black_2  = true;
 let castle_white_1 = true;
@@ -22,6 +27,21 @@ function update_occupied() {
     }
 }
 
+function check_if_in_check(color){
+    if(color == "white") {
+        let king = white_pieces.find(piece => piece.type === "king");
+        if(attacked_squares_black.includes(king.position)) {
+            return true;
+        }
+    } else {
+        let king = black_pieces.find(piece => piece.type === "king");
+        if(attacked_squares_white.includes(king.position)) {
+            return true;
+        }
+    }
+    return false;
+}
+
 function check_valid_move(type, initial_position, final_position, color, id) {
     let initial_row = Number(initial_position[0]);
     let initial_col = Number(initial_position[2]);
@@ -29,6 +49,8 @@ function check_valid_move(type, initial_position, final_position, color, id) {
     let final_col = Number(final_position[2]);
     
     update_occupied()
+
+    in_check = check_if_in_check(color);
 
     if (!(check_no_own_collision(initial_row, initial_col, final_row, final_col, color, final_position, type))) {
         return false;
@@ -216,11 +238,26 @@ function check_valid_bishop_move(initial_row, initial_col, final_row, final_col,
 }
 
 function check_valid_king_move(initial_row, initial_col, final_row, final_col, color, final_position) {
+    
     let diff_row = Math.abs(initial_row - final_row);
     let diff_col = Math.abs(initial_col - final_col);
 
+    if (color == "white") {
+        if (attacked_squares_black.includes(final_position)) {
+            return false;
+        }
+    } else {
+        if (attacked_squares_white.includes(final_position)) {
+            return false;
+        }
+    }
+    console.log("bbbbbbbbbbbbbbbb")
     if (diff_row <= 1 && diff_col <= 1) {
         check_eat(final_position, color)
+        castle_black_1 = false;
+        castle_black_2  = false;
+        castle_white_1 = false;
+        castle_white_2  = false;
         return true;
     }
 
@@ -332,10 +369,7 @@ function check_castling(initial_row, initial_col, final_row, final_col, color, f
         b4_positions = ["8,1", "8,8"];  //rook-left, rook-right
         after_positions = ["8,4", "8,6", "8,3", "8,7"];  //rook-left, rook-right, king, king
     }
-    console.log(castle_white_1)
-    console.log(castle_white_2)
-    console.log(castle_black_1)
-    console.log(castle_black_2)
+
     if (color == "white") {
         if (!(castle_white_1)) {
             b4_positions[0] = "x,x";
@@ -354,8 +388,6 @@ function check_castling(initial_row, initial_col, final_row, final_col, color, f
 
     let rook_ids = [1, 2];
     let index = 0;
-    
-    console.log(b4_positions)
 
 
     if (!(b4_positions.includes(final_position))) {
@@ -403,10 +435,212 @@ function castle (rook_position, king_position, rook_id, color) {
     castling = true;
 }
 
+function calculate_attacked_rook(position, color_pieces) {
+    for (let x = Number(position[0]) + 1; x <= 8; x++) {
+        let next_position = x + "," + position[2];
+        if (occupied_squares_white.includes(next_position) || occupied_squares_black.includes(next_position)) {
+            color_pieces.push(next_position);
+            break;
+        }
+        color_pieces.push(next_position);
+    }
+    for (let x = Number(position[0]) - 1; x >= 1; x--) {
+        let next_position = x + "," + position[2];
+        if (occupied_squares_white.includes(next_position) || occupied_squares_black.includes(next_position)) {
+            color_pieces.push(next_position);
+            break;
+        }
+        color_pieces.push(next_position);
+    }
+    for (let y = Number(position[2]) + 1; y <= 8; y++) {
+        let next_position = position[0] + "," + y;
+        if (occupied_squares_white.includes(next_position) || occupied_squares_black.includes(next_position)) {
+            color_pieces.push(next_position);
+            break;
+        }
+        color_pieces.push(next_position);
+    }
+    for (let y = Number(position[2]) - 1; y >= 1; y--) {
+        let next_position = position[0] + "," + y;
+        if (occupied_squares_white.includes(next_position) || occupied_squares_black.includes(next_position)) {
+            color_pieces.push(next_position);
+            break;
+        }
+        color_pieces.push(next_position);
+    }
+}
+
+function calculate_attacked_bishop(position, color_pieces){
+    let y = 1;
+    for (let x = Number(position[0]) + 1; x <= 8; x++) {
+        let next_position = x + "," + (Number(position[2]) + y);
+        if ((Number(position[2]) + y) > 8) {
+            break;
+        }
+        if (occupied_squares_white.includes(next_position) || occupied_squares_black.includes(next_position)) {
+            color_pieces.push(next_position);
+            break;
+        }
+        y++;
+        color_pieces.push(next_position);
+    }
+    y = 1;
+    for (let x = Number(position[0]) + 1; x <= 8; x++) {
+        let next_position = x + "," + (Number(position[2]) - y);
+        if ((Number(position[2]) - y) < 1) {
+            break;
+        }
+        if (occupied_squares_white.includes(next_position) || occupied_squares_black.includes(next_position)) {
+            color_pieces.push(next_position);
+            break;
+        }
+        y++;
+        color_pieces.push(next_position);
+    }
+    y = 1;
+    for (let x = Number(position[0]) - 1; x >= 1; x--) {
+        let next_position = x + "," + (Number(position[2]) + y);
+        if ((Number(position[2]) + y) > 8) {
+            break;
+        }
+        if (occupied_squares_white.includes(next_position) || occupied_squares_black.includes(next_position)) {
+            color_pieces.push(next_position);
+            break;
+        }
+        y++;
+        color_pieces.push(next_position);
+    }
+    y = 1;
+    for (let x = Number(position[0]) - 1; x >= 1; x--) {
+        let next_position = x + "," + (Number(position[2]) - y);
+        if ((Number(position[2]) - y) < 1) {
+            break;
+        }
+        if (occupied_squares_white.includes(next_position) || occupied_squares_black.includes(next_position)) {
+            color_pieces.push(next_position);
+            break;
+        }
+        y++;
+        color_pieces.push(next_position);
+    }
+}
+
+function calculate_attacked_squares() {
+    attacked_squares_white = [];
+    attacked_squares_black = [];
+    update_occupied()
+    for (let i = 0; i < white_pieces.length; i++) {
+        let position = white_pieces[i].position;
+        switch (white_pieces[i].type) {
+            case "pawn":
+                let attacked1 = (Number(position[0]) + 1) + "," + (Number(position[2]) - 1);
+                let attacked2 = (Number(position[0]) + 1) + "," + (Number(position[2]) + 1);
+                attacked_squares_white.push(attacked1);
+                attacked_squares_white.push(attacked2);
+                break;
+            case "rook":
+                calculate_attacked_rook(position, attacked_squares_white);
+                break;
+            case "knight":
+                for (let x = -2; x <= 2; x++) {
+                    for (let y = -2; y <= 2; y++) {
+                        if ((!(Math.abs(x) - Math.abs(y) == 0))&& (x != 0) && (y != 0)) {
+                            let attacked = (Number(position[0]) + x) + "," + (Number(position[2]) + y);
+                            if (attacked != position) {
+                                attacked_squares_white.push(attacked);
+                            }
+                        }
+
+                    }
+                }
+                break;
+            case "bishop":
+                calculate_attacked_bishop(position, attacked_squares_white);
+                break;
+            case "queen":
+                calculate_attacked_rook(position, attacked_squares_white);
+                calculate_attacked_bishop(position, attacked_squares_white);
+                break;
+            case "king":
+                for (let x = -1; x <= 1; x++) {
+                    for (let y = -1; y <= 1; y++) {
+                        let attacked = (Number(position[0]) + x) + "," + (Number(position[2]) + y);
+                        if (attacked != position) {
+                            attacked_squares_white.push(attacked);
+                        }
+                    }
+                }
+                break;
+        }
+    }
+    for (let i = 0; i < black_pieces.length; i++) {
+        let position = black_pieces[i].position;
+        switch (black_pieces[i].type) {
+            case "pawn":
+                let attacked1 = (Number(position[0]) - 1) + "," + (Number(position[2]) - 1);
+                let attacked2 = (Number(position[0]) - 1) + "," + (Number(position[2]) + 1);
+                attacked_squares_black.push(attacked1);
+                attacked_squares_black.push(attacked2);
+                break;
+            case "rook":
+                calculate_attacked_rook(position, attacked_squares_black);
+                break;
+            case "knight":
+                for (let x = -2; x <= 2; x++) {
+                    for (let y = -2; y <= 2; y++) {
+                        if ((!(Math.abs(x) - Math.abs(y) == 0))&& (x != 0) && (y != 0)) {
+                            let attacked = (Number(position[0]) + x) + "," + (Number(position[2]) + y);
+                            if (attacked != position) {
+                                attacked_squares_black.push(attacked);
+                            }
+                        }
+
+                    }
+                }
+                break;
+            case "bishop":
+                calculate_attacked_bishop(position, attacked_squares_black);
+                break;
+            case "queen":
+                calculate_attacked_rook(position, attacked_squares_black);
+                calculate_attacked_bishop(position, attacked_squares_black);
+                break;
+            case "king":
+                for (let x = -1; x <= 1; x++) {
+                    for (let y = -1; y <= 1; y++) {
+                        let attacked = (Number(position[0]) + x) + "," + (Number(position[2]) + y);
+                        if (attacked != position) {
+                            attacked_squares_black.push(attacked);
+                        }
+                    }
+                }
+                break;
+        }
+    }
+    attacked_squares_white = [...new Set(attacked_squares_white)];
+    attacked_squares_white = attacked_squares_white.filter(coord => !coord.includes('0'));
+    attacked_squares_white = attacked_squares_white.filter(coord => !coord.includes('-'));
+    attacked_squares_black = [...new Set(attacked_squares_black)];
+    attacked_squares_black = attacked_squares_black.filter(coord => !coord.includes('0'));
+    attacked_squares_black = attacked_squares_black.filter(coord => !coord.includes('-'));
+
+    let squares = document.querySelectorAll('.square');
+    for (let square of squares) {
+        square.removeAttribute('style');
+    }
+
+    for (let id of attacked_squares_white) {
+        let square = document.getElementById(id);
+        if (square) {
+            square.style.setProperty("background-color", "rgba(248, 103, 103, 0.5)", "important");
+        }
+    }
+
+}
 
 function play_move_sound() {
     if (move_sound) {
-        move_sound.currentTime = 0; // restart sound if already playing
+        move_sound.currentTime = 0;
         move_sound.play();
     }
 }
